abdc83864b88401c1e13cdefd6a52fcc57d28a6b
Juanma <juanmanuelruizgil@gmail.com>
2020-12-15 20:25:21 +0100

    Pruebas

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #CODIGO PARA EL DESARROLLO DE UNA CALCULADORA EN PYTHON
+from tkinter import *
+
+root =Tk()
+
+mainFrame = Frame(root)
+mainFrame.pack()
+root.title("Calculator by Juanma")
+root.mainloop()
